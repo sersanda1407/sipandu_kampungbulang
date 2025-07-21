8fa892a2c264b86f3b86a62f7c53ad91fa7b6f11
sersanda1407 <sersandabagas8@gmail.com>
2025-07-21 14:56:36 +0700

    update

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -65,7 +65,7 @@
         <div class="modal-dialog">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel">Edit Akun</h5>
+                    <h5 class="modal-title" id="exampleModalLabel">Kelola Akun</h5>
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <form action="/edit-profile" method="POST">
